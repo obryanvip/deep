34e846b7afa44f071645928dbf960871bb547092
obryanvip <31418214+obryanvip@users.noreply.github.com>
2023-10-28 12:10:02 -0400

    studio.sh

--- studio.sh
+++ studio.sh
@@ -1,5 +1,7 @@
+%%shell
+#!/bin/bash
+sudo apt update > /dev/null > 0
 
-sudo apt update
 sudo apt install default-jdk -y
 sudo apt install curl -y
 curl -L -o android-studio-2022.3.1.19-linux.tar.gz \https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.19/android-studio-2022.3.1.19-linux.tar.gz
