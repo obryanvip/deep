c49e245171ee6c0885a364b1faa70d70aa14a925
obryanvip <31418214+obryanvip@users.noreply.github.com>
2023-10-28 13:16:44 -0400

    studiosudo.sh

--- studiosudo.sh
+++ studiosudo.sh
@@ -1,14 +1,9 @@
 
-sudo apt update
-sudo apt install default-jdk -y
-sudo apt install curl -y
-curl -L -o android-studio-2022.3.1.19-linux.tar.gz \https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.19/android-studio-2022.3.1.19-linux.tar.gz
-
-
+sudo apt update ;
+sudo apt install default-jdk -y ;
+sudo apt install curl -y ;
+curl -L -o android-studio-2022.3.1.19-linux.tar.gz \https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.19/android-studio-2022.3.1.19-linux.tar.gz ;
 #https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.19/android-studio-2022.3.1.19-linux.tar.gz
-
 #android-studio-2022.3.1.19-linux.tar.gz
-
-sudo tar -xvf android-studio-*.*-linux.tar.gz -C /usr/local/
-
+sudo tar -xvf android-studio-*.*-linux.tar.gz -C /usr/local/;
 #ejecutar desde el entorno grafico !sudo sh /usr/local/android-studio/bin/studio.sh
