c25c215ae7d58080e3f988427745d105cd94514a
obryanvip <31418214+obryanvip@users.noreply.github.com>
2023-10-28 13:05:00 -0400

    googlesudo.sh

--- googlesudo.sh
+++ googlesudo.sh
@@ -2,11 +2,12 @@ sudo apt update > /dev/null
 sudo apt install --assume-yes --fix-broken curl #administrador de archivos
 curl -L -o chrome-remote-desktop_current_amd64.deb \https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo DEBIAN_FRONTEND=noninteractive \apt-get install --assume-yes --fix-broken ./chrome-remote-desktop_current_amd64.deb
-  sudo apt install --assume-yes --fix-broken 
+sudo apt install --assume-yes --fix-broken 
 curl -L -o google-chrome-stable_current_amd64.deb \https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo DEBIAN_FRONTEND=noninteractive \apt-get install --assume-yes --fix-broken ./google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken konsole
-sudo apt install --assume-yes --fix-broken  firefoxsudo apt install --assume-yes --fix-broken  chromium
+sudo apt install --assume-yes --fix-broken  firefox
+sudo apt install --assume-yes --fix-broken  chromium
 sudo apt install --assume-yes --fix-broken thunar #administrador de archivos
 #novnc local
 
