e3531c7f417d8dfcb44f6e75a73bc153fd658bb9
obryanvip <31418214+obryanvip@users.noreply.github.com>
2023-12-12 11:10:38 -0400

    Update graficos1.sh

--- graficos1.sh
+++ graficos1.sh
@@ -1,1 +1,23 @@
-./noVNC/utils/launch.sh --listen 8080 --vnc localhost:5901
+#!/bin/bash
+echo '#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &' > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
+
+
+sudo apt install xfce4 xfce4-goodies -y
+sudo apt-get install tightvncserver
+
+
+
+sudo mkdir -p ~/.vnc && echo '#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &' > ~/.vnc/xstartup && chmod +x ~/.vnc/xstartup
+
+
+
+#!  sudo vncserver -geometry 1440x900
+
+git clone https://github.com/ayunami2000/noVNC
+#./noVNC/utils/launch.sh --listen 8080 
+#./noVNC/utils/launch.sh --listen 8080 --vnc localhost:5901
