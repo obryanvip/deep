2530863cda4df57b0ac79a285b88976edec74510
obryanvip <31418214+obryanvip@users.noreply.github.com>
2023-12-14 13:46:12 -0400

    Update andriod.sh

--- andriod.sh
+++ andriod.sh
@@ -2,6 +2,8 @@ docker run -d -p 6080:6080 -e EMULATOR_DEVICE="Samsung Galaxy S10" -e WEB_VNC=tr
 
 docker run -d -p 6080:6080 -e EMULATOR_DEVICE="Samsung Galaxy S10" -e WEB_VNC=true --device /dev/kvm --name obry  budtmo/docker-android:emulator_9.0
 
+run --privileged -d
+
 docker exec -it obry adb emu sms send 04249470620 23232
 
 
