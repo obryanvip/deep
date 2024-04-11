ef5150271e0d75e2b2a4b927648e30273cf15984
obryanvip <31418214+obryanvip@users.noreply.github.com>
2024-04-11 19:02:31 +0000

    Update andriod.sh

--- andriod.sh
+++ andriod.sh
@@ -50,6 +50,8 @@ docker run --privileged -d -p 6080:6080 -e EMULATOR_DEVICE="Samsung Galaxy S10" 
 ocker run -d -p 6080:6080 -e EMULATOR_DEVICE="Samsung Galaxy S10" -e WEB_VNC=true --device /dev/kvm --name obry  budtmo/docker-android:emulator_9.0
 
 docker run --privileged -d -e EMULATOR_DEVICE="Samsung Galaxy S10" -e WEB_VNC=true --device /dev/kvm --name obry  budtmo/docker-android:emulator_9.0
+docker run -d -p 6080:6080 -e EMULATOR_DEVICE="Samsung Galaxy S10" -e WEB_VNC=true --device /dev/kvm obryanvip/emu2024
+
 
 
 
