23021ffc0980a4931a4dc15772abc3acf82e2519
obryanvip <31418214+obryanvip@users.noreply.github.com>
2025-02-14 13:53:46 -0400

    Add files via upload

---
+++ firefox.sh
@@ -0,0 +1,8 @@
+sudo wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/126.0.1/linux-x86_64/es-ES/firefox-126.0.1.tar.bz2;
+sudo tar xjf firefox-*.tar.bz2;
+sudo  cp -r firefox /opt
+sudo mv firefox /opt
+sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
+
+
