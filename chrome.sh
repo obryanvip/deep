9237788121a6ee177eee597371c71ec0da39f8cf
obryanvip <31418214+obryanvip@users.noreply.github.com>
2025-03-11 14:37:18 +0000

    Add files via upload

---
+++ chrome.sh
@@ -0,0 +1,3 @@
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+sudo dpkg --install google-chrome-stable_current_amd64.deb;
+sudo apt install --assume-yes --fix-broken;
