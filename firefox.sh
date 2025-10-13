22a8e7d743f2ef38e5a41351c59aa81cc0bf4280
Obryan Alfonso Lopez Aparicio <31418214+obryanvip@users.noreply.github.com>
2025-10-13 09:42:01 -0400

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -6,3 +6,10 @@ sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
 wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
 
 
+de code space github
+
+wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/140.0esr/linux-x86_64/es-ES/firefox-140.0esr.tar.xz
+tar -xf firefox-*.tar.xz
+
+
+
