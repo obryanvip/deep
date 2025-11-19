15e2dc256621ea8b8f892c4c2fbbe8f978101884
Obryan Alfonso Lopez Aparicio <31418214+obryanvip@users.noreply.github.com>
2025-11-19 17:30:32 +0000

    Add alternative download link for VS Code
    Added an alternative download link for Visual Studio Code.

--- vscodeinstall.sh
+++ vscodeinstall.sh
@@ -1,4 +1,5 @@
 wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/cb1933bbc38d329b3595673a600fab5c7368f0a7/code_1.106.1-1763289752_amd64.deb
 sudo dpkg -i ./code_*.deb
 sudo apt-get install -f
 
@@ -13,7 +14,7 @@ sudo apt install wget gpg
 #wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 #sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/packages.microsoft.gpg
 
-   
+   ####si no instala descargar desde el navegador y instalar con estos comandos
         
 
 
