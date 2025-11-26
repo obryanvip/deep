a6afedc0af2e09e4956bd76944df3e33922468c7
Obryan Alfonso Lopez Aparicio <31418214+obryanvip@users.noreply.github.com>
2025-11-26 11:55:55 -0400

    Update mover.sh

--- mover.sh
+++ mover.sh
@@ -1,1 +1,11 @@
-mv /tmp/nubecodespace/todo/home/* /home/codespace 
+mv /tmp/nubecodespace/todo/home/* /home/codespace/ 
+
+
+mv /tmp/nubecodespace/todo/home/.* /home/codespace/
+
+sudo rm -r  /opt/goole/
+
+mv /tmp/nubecodespace/todo/opt/. /opt
+
+mv /tmp/nubecodespace/todo/opt/.* /opt
+
