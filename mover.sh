bff885dcae4264fbb0ba492c910f6123960f747c
Obryan Alfonso Lopez Aparicio <31418214+obryanvip@users.noreply.github.com>
2025-11-26 14:21:47 -0400

    Update mover.sh

--- mover.sh
+++ mover.sh
@@ -42,13 +42,15 @@ mv /tmp/nubecodespace/todo/home-codespace/.android \
 
 #########desde deep
 ls /home/codespace/
-sudo rm -r  /home/codespace/  ; sudo rm -r  /home/codespace/ ; sudo rm -r  /home/codespace/  ; sudo rm -r  /home/codespace/ 
+sudo rm -r  /home/codespace/  ; sudo rm -r  /home/codespace/ ; sudo rm -r  /home/codespace/  ; sudo rm -r  /home/codespace/  mkdir -p /home/codespace ;
 mv /workspaces/deep/nubecodespace/todo/home/* /home/codespace/
 
 
 /opt/google/chrome-remote-desktop/chrome-remote-desktop --start 
 
+enlace 
 
+ln -s /home/codespace /workspaces/deep/enlace
 
 
 
