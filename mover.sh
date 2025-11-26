23dc8d7c5581d289e98c6a220089310ffbca2c48
Obryan Alfonso Lopez Aparicio <31418214+obryanvip@users.noreply.github.com>
2025-11-26 13:27:47 -0400

    Update mover.sh

--- mover.sh
+++ mover.sh
@@ -17,6 +17,27 @@ ls /tmp/nubecodespace/todo/opt
 ls /tmp/nubecodespace/todo/home/
 
 
+/tmp/nubecodespace/todo/home-codespace/.android
+/tmp/nubecodespace/todo/home-codespace/.cache
+/tmp/nubecodespace/todo/home-codespace/.conda
+/tmp/nubecodespace/todo/home-codespace/.config
+/tmp/nubecodespace/todo/home-codespace/.local
+/tmp/nubecodespace/todo/home-codespace/.mozilla
+/tmp/nubecodespace/todo/home-codespace/.vscode
+/tmp/nubecodespace/todo/home-codespace/.vscode-remote
+
+
+#####copiaar estas carpetas especificas
+mv /tmp/nubecodespace/todo/home-codespace/.android \
+/tmp/nubecodespace/todo/home-codespace/.cache \
+/tmp/nubecodespace/todo/home-codespace/.conda \
+/tmp/nubecodespace/todo/home-codespace/.config \
+/tmp/nubecodespace/todo/home-codespace/.local \
+/tmp/nubecodespace/todo/home-codespace/.mozilla \
+/tmp/nubecodespace/todo/home-codespace/.vscode \
+/tmp/nubecodespace/todo/home-codespace/.vscode-remote \
+/home/codespace
+
 
 
 
