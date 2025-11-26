a101dd423c3ea4336c9cd480b028b177f093c9de
Obryan Alfonso Lopez Aparicio <31418214+obryanvip@users.noreply.github.com>
2025-11-26 17:16:11 -0400

    Create androidstudio2025.sh

---
+++ androidstudio2025.sh
@@ -0,0 +1,4 @@
+
+wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2025.2.1.8/android-studio-2025.2.1.8-linux.tar.gz
+
+https://r3---sn-p5qlsn6s.gvt1.com/edgedl/android/studio/ide-zips/2025.2.1.8/android-studio-2025.2.1.8-linux.tar.gz?cms_redirect=yes&met=1764191663,&mh=qA&mip=172.210.53.224&mm=28&mn=sn-p5qlsn6s&ms=nvh&mt=1764190396&mv=u&mvi=3&pl=20&rmhost=r1---sn-p5qlsn6s.gvt1.com&rms=nvh,nvh&shardbypass=sd&smhost=r2---sn-qjp5q5-55.gvt1.com
