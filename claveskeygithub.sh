ed73f5a7ed7776aea9dc37af7f926fbb0d0c1fd4
Obryan Alfonso Lopez Aparicio <31418214+obryanvip@users.noreply.github.com>
2026-06-16 22:23:35 -0400

    Update claveskeygithub.sh

--- claveskeygithub.sh
+++ claveskeygithub.sh
@@ -1,5 +1,5 @@
 sudo apt update ; sudo apt install -y gnome-keyring seahorse libsecret-1-0 dbus-x11 ; mkdir -p ~/.local/share/keyrings ; echo -e "[keyring]\ndisplay-name=login\nctime=0\nmtime=0\nlock-on-idle=false\nlock-after=false" > ~/.local/share/keyrings/login.keyring ; echo "login" > ~/.local/share/keyrings/default ; killall gnome-keyring-daemon 2>/dev/null ; export $(dbus-launch --sh-syntax) ; eval $(gnome-keyring-daemon --start --components=secrets)
-
+mkdir -p ~/.local/share/keyrings ; echo -e "[keyring]\ndisplay-name=login\nctime=0\nmtime=0\nlock-on-idle=false\nlock-after=false" > ~/.local/share/keyrings/login.keyring ; echo "login" > ~/.local/share/keyrings/default ; killall gnome-keyring-daemon 2>/dev/null ; killall github-desktop 2>/dev/null ; eval $(dbus-launch --sh-syntax) ; eval $(gnome-keyring-daemon --start --components=secrets)
 sudo apt update && sudo apt install -y gnome-keyring seahorse libsecret-1-0 dbus-x11 && \
 mkdir -p ~/.local/share/keyrings && \
 cat <<EOF > ~/.local/share/keyrings/login.keyring
